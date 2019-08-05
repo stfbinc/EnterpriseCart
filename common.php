8a7044bacebeb327d223402b4644e02ffb2f454f
ixdu <ix@2du.ru>
2019-08-05 19:12:20 +0500

    changed database name to enterprise

--- common.php
+++ common.php
@@ -8,7 +8,7 @@ function config(){
         "db_host" => "localhost",
         "db_user" => "enterprise",
         "db_password" => "enterprise",
-        "db_base" => "myenterprise",
+        "db_base" => "enterprise",
         "loginLogo" => "assets/images/stfb-logo.gif",
         "mediumLogo" => "assets/images/stfb-logo.gif",
         "smallLogo" => "assets/images/stfblogosm.jpg",
