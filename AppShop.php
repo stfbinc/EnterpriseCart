bc732b689f87e24552e122f11d08190c5cbaa419
ixdu <ix@2du.ru>
2019-09-24 20:54:25 +0500

    [Documentation] added comments to AppShop.php config

--- AppShop.php
+++ AppShop.php
@@ -1,24 +1,24 @@
 <?php
 function config(){
     return array(
-        "theme" => 'none',
+        "theme" => 'none', 
         //"theme" => 'dark', uncomment for dark theme
-        "title" => 'Enterprise X Hosting Cart',
-        "software" => "HostingCart",
-        "loginForm" => "login",
-        "db_host" => "localhost",
-        "db_user" => "enterprise",
-        "db_password" => "enterprise",
-        "db_base" => "enterpriseadmin",
+        "title" => 'Enterprise X Hosting Cart', //name of Application
+        "software" => "HostingCart", //type of software, can be HostingCart or Cart
+        "loginForm" => "login", //not used now
+        "db_host" => "localhost", //host of mysql database
+        "db_user" => "enterprise", //user of mysql database
+        "db_password" => "enterprise", //password of mysql database user
+        "db_base" => "enterpriseadmin",//name of mysql database
         "loginLogo" => "assets/images/stfb-logo.gif",
         "mediumLogo" => "assets/images/stfb-logo.gif",
         "smallLogo" => "assets/images/stfblogosm.jpg",
-        "defaultCompanyID" => "DINOS",
-        "defaultDivisionID" => "DEFAULT",
-        "defaultDepartmentID" => "DEFAULT",
-        "EnterpriseXURL" => "/EnterpriseX",
-        "EnterpriseXEmployeeID" => "Demo",
-        "EnterpriseXEmployeePassword" => "Demo"
+        "defaultCompanyID" => "DINOS", //CompanyID for getting items from database
+        "defaultDivisionID" => "DEFAULT",//DivisionID for getting items from database
+        "defaultDepartmentID" => "DEFAULT",//DepartmentID for getting items from database
+        "EnterpriseXURL" => "/EnterpriseX", //path to EnterpriseX application, it must be on same host for now
+        "EnterpriseXEmployeeID" => "Demo", //name of EnterpriseX user
+        "EnterpriseXEmployeePassword" => "Demo" // password for EnterpriseX user
     );
 }
 
